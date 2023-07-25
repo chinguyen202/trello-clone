7cae047753c7425666da248685bdb6fd908ba387
Chi Nguyen <chi.nguyen@live.com>
2023-07-25 13:13:08 +0300

    add style component for column, card, column title

--- src/styles.ts
+++ src/styles.ts
@@ -9,3 +9,28 @@ export const AppContainer = styled.div`
   padding: 20px;
   width: 100%;
 `;
+
+export const ColumnContainer = styled.div`
+  background-color: #03045e;
+  width: 300px;
+  min-height: 50px;
+  margin-right: 20px;
+  border-radius: 3px;
+  padding: 8px 8px;
+  flex-grow: 0;
+`;
+
+export const ColumnTitle = styled.div`
+  padding: 5px 15px 12px;
+  font-weight: bold;
+`;
+
+export const CardContainer = styled.div`
+  background-color: #fff;
+  cursor: pointer;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  max-width: 300px;
+  border-radius: 3px;
+  box-shadow: #091e4240 0px 1px 0px 0px;
+`;
