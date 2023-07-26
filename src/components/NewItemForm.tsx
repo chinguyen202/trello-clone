fd3e885fd3939cceaef8d3fa15c6d3ccf72e45c0
Chi Nguyen <chi.nguyen@live.com>
2023-07-27 00:04:12 +0300

    create form to add new item

---
+++ src/components/NewItemForm.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { NewItemButton, NewItemContainer, NewItemInput } from '../styles';
+
+type NewItemFormProps = {
+  onAdd(text: string): void;
+};
+
+export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
+  const [text, setText] = useState('');
+  return (
+    <NewItemContainer>
+      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
+    </NewItemContainer>
+  );
+};
